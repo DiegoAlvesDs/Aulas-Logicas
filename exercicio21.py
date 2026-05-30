6b744f31ddee1283961072196d0b3f43338b0ed5
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-05-30 18:31:01 -0300

    Delete exercicio21.py

--- exercicio21.py
+++
@@ -1,8 +0,0 @@
-turno = input ("em que turno voçê estuda?(M-matutino / V-vespertino / N-noturno)")
-
-if turno == "M" or turno == "M":
-   print("Bom Dia")
-elif turno == "V" or turno == "v":
-   print("Boa Tarde")
-else: 
-   print("valor invalido")
