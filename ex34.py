78dca614ab7fd83a5bb1c12e35dc87f06a81f602
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-05-30 18:28:42 -0300

    Delete ex34.py

--- ex34.py
+++
@@ -1,9 +0,0 @@
-def media(lista):
-    return sum(lista) / len(lista)
-
-notas = [float(input(f"Nota {i+1}: ")) for i in range(4)]
-
-print("\nNotas digitadas:")
-print(*notas)
-
-print(f"Média = {media(notas):.2f}")
