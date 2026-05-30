1fe8ced1965e7c337c71e224a5e82a2184a3570a
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-05-30 18:30:36 -0300

    Delete exercicio19.py

--- exercicio19.py
+++
@@ -1,7 +0,0 @@
-saldo = float(input("Digite seu saldo: "))
-saque = float(input("Digite o valor do saque: "))
-deposito = float(input("Digite o valor do depósito: "))
-
-saldo = saldo - saque + deposito
-
-print("Saldo final:", saldo)
