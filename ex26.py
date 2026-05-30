534c748fa7893d21133e466f3d55e72a0b0cdc50
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-05-30 18:29:00 -0300

    Delete ex26.py

--- ex26.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Digite um número: "))
-
-print(f"\n=== TABUADA DO {numero} ===\n")
-
-for i in range(1, 11):
-    resultado = numero * i
-    print(f"{numero} x {i} = {resultado}")
