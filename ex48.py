cc0d551ba584fd141cb17dd15968e1481a32d2cb
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 06:29:05 -0300

    Organiza: remove ex48.py da raiz (movido)

--- ex48.py
+++
@@ -1,7 +0,0 @@
-def potencia_eletrica():
-    e = float(input("Digite a energia (J): "))
-    t = float(input("Digite o tempo (s): "))
-    p = e / t
-    print("Potência =", p, "W")
-
-potencia_eletrica()
