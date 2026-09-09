a7023ba05300ec1ba781a2d3b17cd0cf8b1cd75b
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 06:28:19 -0300

    Organiza: remove ex44.py da raiz (movido)

--- ex44.py
+++
@@ -1,7 +0,0 @@
-def energia_cinetica():
-    m = float(input("Digite a massa (kg): "))
-    v = float(input("Digite a velocidade (m/s): "))
-    ec = (m * v**2) / 2
-    print("Energia Cinética =", ec, "J")
-
-energia_cinetica()
